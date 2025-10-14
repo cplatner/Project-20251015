66be4c66fdcf69cf85327b7be9332f7def6671c8
Chris Platner <chris@platner.com>
2025-10-14 15:34:31 -0600

    - Add testing

---
+++ backend/StargateAPI.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace StargateAPI.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ backend/StargateAPI.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace StargateAPI.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
